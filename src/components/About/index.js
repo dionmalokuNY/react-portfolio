529ccbc5fcfc76a309aeacb5e4ede2a26f3fbfad
dionmalokuNY <dionn.maloku@outlook.com>
2022-04-25 02:39:31 +0200

    finished first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react'
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm very ambitious front-end developer looking for a role in
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, a gaming fanatic, and tech-obsessed!!!
+          </p>
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['M', 'y', ' ', 'p', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            <a href='https://diondirectory.netlify.app/'><p>Directory Web created with Bootstrap and Sass ( Click on me)</p></a>
+            <a href='https://dionfinsweet.netlify.app/'><p>Finsweet Web created with Vanilla Html and Css ( Click on me)</p></a>
+            And the Portfolio was created with ReactJS/Gsap/Sass/Loader.css/Animate.css. More projects on the way stay tuned :)
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faPython} color="#FFDE57" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
